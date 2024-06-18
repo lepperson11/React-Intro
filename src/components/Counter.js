922b16db76977a643e5e0869c34a0872fc929ff9
lepperson11 <lepperson11@gmail.com>
2024-06-18 12:42:37 -0600

    first commit

---
+++ src/components/Counter.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./counter.css";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const handleDecement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+  return (
+    <div className="container">
+      <div>
+        <button className="btn btn-3" onClick={handleDecement}>
+          Minus 1
+        </button>
+        <button className="btn btn-2" onClick={handleIncrement}>
+          Add 1
+        </button>
+
+        <h3 className="counter">Count is {count}</h3>
+        <button
+          className="btn btn-3"
+          onClick={() => {
+            setCount(0);
+          }}
+        >
+          Reset Counter
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
